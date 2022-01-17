e20c2496c6d3bea723ef3785cc2d7f253fbd4bee
Shogo Tsuneto <shogotsuneto@gmail.com>
2022-01-17 15:16:01 +0900

    feat: enable single nat

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -47,3 +47,8 @@ variable "vpc_public_subnets" {
   type    = list(string)
   default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
+
+variable "vpc_single_nat_gateway" {
+  type    = bool
+  default = true
+}
