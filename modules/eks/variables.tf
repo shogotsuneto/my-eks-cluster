138c22a8de8009b801d73a377fd76613dc39a953
Shogo Tsuneto <shogotsuneto@gmail.com>
2022-01-17 15:19:04 +0900

    feat: undefault vpc subnets

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -34,18 +34,15 @@ variable "vpc_azs" {
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  type = string
 }
 
 variable "vpc_private_subnets" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  type = list(string)
 }
 
 variable "vpc_public_subnets" {
-  type    = list(string)
-  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  type = list(string)
 }
 
 variable "vpc_single_nat_gateway" {
