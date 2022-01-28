67a867a59e04485d81bbff4e43c0f5747a3dd88f
Shogo Tsuneto <shogotsuneto@gmail.com>
2022-01-28 19:10:10 +0900

    feat: use primary cluster sg

--- modules/eks/cluster.tf
+++ modules/eks/cluster.tf
@@ -2,6 +2,7 @@ module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
   create                                = local.create
+  create_cluster_security_group         = false
   cluster_name                          = local.cluster_name
   cluster_additional_security_group_ids = var.cluster_additional_security_group_ids
   cluster_endpoint_private_access       = false # default
@@ -12,7 +13,4 @@ module "eks" {
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = local.public_subnets_only ? module.vpc.public_subnets : module.vpc.private_subnets
-
-  eks_managed_node_group_defaults = var.eks_managed_node_group_defaults
-  eks_managed_node_groups         = var.eks_managed_node_groups
 }
