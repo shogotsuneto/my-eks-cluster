b44a7692bce926404831bb04da7ba2bda5566f66
Shogo Tsuneto <shogotsuneto@gmail.com>
2022-02-02 20:20:12 +0900

    fix: mapRoles for separate node group

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -14,8 +14,12 @@ output "autoscaler_sa_name" {
   value = local.autoscaler_sa_name
 }
 
+locals {
+  alertmanager_role_arn = module.iam_assumable_role_alertmanager.iam_role_arn
+}
+
 output "alertmanager_role_arn" {
-  value = module.iam_assumable_role_alertmanager.iam_role_arn
+  value = local.alertmanager_role_arn
 }
 
 output "alertmanager_sa_name" {
@@ -24,4 +28,27 @@ output "alertmanager_sa_name" {
 
 output "alertmanager_topic_arn" {
   value = aws_sns_topic.alertmanager.arn
+}
+
+output "map_roles" {
+  # used aws_auth_configmap_yaml of the original eks module as reference:
+  # https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/outputs.tf#L164
+  value = join("\n", compact(flatten([
+    [for group in module.eks_managed_node_group : trimspace(templatefile(
+      "${path.module}/templates/mapRoles/eks_managed_node.tpl.yaml",
+      { ROLE_ARN = group.iam_role_arn }
+    ))],
+    [for group in module.eks.fargate_profiles : trimspace(templatefile(
+      "${path.module}/templates/mapRoles/fargate_profile.tpl.yaml",
+      { ROLE_ARN = group.fargate_profile_arn }
+    ))],
+    local.alertmanager_role_arn != "" ? trimspace(templatefile( # alertmanager
+      "${path.module}/templates/mapRoles/serviceaccount.tpl.yaml",
+      {
+        ROLE_ARN       = local.alertmanager_role_arn
+        NAMESPACE      = local.alertmanager_namespace
+        SERVICEACCOUNT = local.alertmanager_sa_name
+      }
+    )) : ""
+  ])))
 }
